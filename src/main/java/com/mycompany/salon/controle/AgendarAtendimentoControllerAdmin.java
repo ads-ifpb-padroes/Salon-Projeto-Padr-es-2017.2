0e99ef6342affc4de48f7d39f0a0ad907d5e6537
alexalins <alexa.glinsoo@gmail.com>
2018-04-14 05:23:16 -0300

    controlador do agendar feito

--- src/main/java/com/mycompany/salon/controle/AgendarAtendimentoControllerAdmin.java
+++ src/main/java/com/mycompany/salon/controle/AgendarAtendimentoControllerAdmin.java
@@ -14,7 +14,6 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
--- src/main/java/com/mycompany/salon/persistencia/AtendimentoDao.java
+++ src/main/java/com/mycompany/salon/persistencia/AtendimentoDao.java
@@ -23,7 +23,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
